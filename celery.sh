77dc10795c68f880c78dc2b189abf6ee4ac54360
Artur <artur@tartigrado.com>
2025-10-16 15:09:08 -0300

    fix: fixed concurrency

--- celery.sh
+++ celery.sh
@@ -3,4 +3,4 @@ set -a
 source .env
 set +a
 celery -A tasks.celery_app worker -Q main --concurrency=4 --loglevel=INFO -n worker_main@%h & \
-celery -A tasks.celery_app worker -Q trf --concurrency=8 --loglevel=INFO -n worker_trf@%h
+celery -A tasks.celery_app worker -Q trf --concurrency=1 --loglevel=INFO -n worker_trf@%h
